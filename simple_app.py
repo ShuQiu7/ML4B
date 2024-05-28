a6ecf1b717bd8ce8b05242204b1c24a08c30c86e
felixroeder <167775998+felixroeder@users.noreply.github.com>
2024-05-28 17:05:18 +0000

    commit

--- simple_app.py
+++ simple_app.py
@@ -6,9 +6,14 @@ st.title("Stock Prediction App")
 st.subheader('This app is created to forecast the stock market movement using daily news headlines')
 st.image("https://t4.ftcdn.net/jpg/02/20/32/75/240_F_220327557_gRDTuYL4iVG0lWrjgjrv1chBCUunjKlG.jpg")
 
-news_headline = st.text_input("Enter News Headline")
+st.session_state.News = ""
+st.session_state.Price = ""
 
-stockprice_yesterday = st.text_input("Closing Stock Price From Yesterday")
+news_headline = st.text_input("Enter News Headline")
+stockprice_yesterday = st.text_input("Yesterdays closing Stock Price")
+
+st.session_state.News = news_headline
+st.session_state.Price = stockprice_yesterday
 
 data_load_state = st.text("Load data...")
 
